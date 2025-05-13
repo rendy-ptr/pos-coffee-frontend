0354ef67b787e0108b3088d1f8bc974e9ea83398
Rendy-ptr <rendyp348@gmail.com>
2025-05-13 15:26:25 +0700

    Test Husky setup

--- src/layouts/LandingPageLayout.tsx
+++ src/layouts/LandingPageLayout.tsx
@@ -1,10 +1,8 @@
 const LandingPageLayout = () => {
   return (
-    <div>
-      <h1 className="text-2xl text-center text-emerald-600">
-        Testing Landing Page!
-      </h1>
-    </div>
+    <h1 className="text-2xl text-center                  text-emerald-600">
+      Testing Landing Page!
+    </h1>
   );
 };
 
