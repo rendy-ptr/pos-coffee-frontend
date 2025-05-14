aa3c76ac121b6f6bc3f8e2d1d949124c7bc9f8ba
Rendy-ptr <rendyp348@gmail.com>
2025-05-14 13:30:25 +0700

    Enforce LF line endings with .gitattributes

---
+++ src/sections/HeroSection.tsx
@@ -0,0 +1,11 @@
+import { motion } from 'motion/react';
+
+const HeroSection = () => {
+  return (
+    <motion.section>
+      <h1> Ini Hero Section </h1>
+    </motion.section>
+  );
+};
+
+export default HeroSection;
